1d3e583f16fb5d9d41f905c323c5a3561de1f740
Piyush Agarwal <70555785+piyushag0611@users.noreply.github.com>
2023-05-30 11:35:33 -0700

    Update similarity.py

--- vector_semantics/similarity.py
+++ vector_semantics/similarity.py
@@ -1,1 +1,51 @@
+import gensim.downloader
+import pandas as pd
+import numpy as np
+from sys import exit
 
+def generate_similarity_index(file_name, type_):
+  glove_vectors_wiki = gensim.downloader.load('glove-wiki-gigaword-50')
+  glove_vectors_twitter = gensim.downloader.load('glove-twitter-50')
+
+  def cos_wiki(word1, word2):
+      vec1 = glove_vectors_wiki[word1]
+      vec2 = glove_vectors_wiki[word2]
+      return np.dot(vec1, vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
+
+  def cos_twitter(word1, word2):
+      vec1 = glove_vectors_twitter[word1]
+      vec2 = glove_vectors_twitter[word2]
+      return np.dot(vec1, vec2)/(np.linalg.norm(vec1)*np.linalg.norm(vec2))
+  
+  if(type=='xlsx'):
+    data = pd.read_excel(file_name)
+  elif(type=='csv'):
+    data = pd.read_csv(file_name)
+  else:
+    exit("Wrong File Format")
+  
+  value_pairs1 = []
+  value_pairs2 = []
+  for i in range(len(data)):
+      word1 = data.iloc[i]['words1'].split()[0].lower()
+      word2 = data.iloc[i]['words2'].split()[0].lower()
+      print(word1, word2)
+      try:
+          val1 = cos_wiki(word1, word2)
+      except:
+          val1 = -2
+      try:
+          val2 = cos_twitter(word1, word2)
+      except:
+          val2 = -2
+          
+  value_pairs1.append(val1)
+  value_pairs2.append(val2)
+  data['Glove-50-Wiki Measure'] = value_pairs1
+  data['Glove-50-Twitter Measure'] = value_pairs2
+  if(type=='xlsx'):
+    data.to_excel(file_name)
+  elif(type=='csv'):
+    data.to_csv(file_name)
+
+    
